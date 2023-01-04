a457fc169403b1b565aa59aaf31b13844cd4a4c0
Hasyim Kai <ichihatsu1@gmail.com>
2023-01-04 20:40:30 +0700

    Add Global Slice

--- app/GlobalSlice.ts
+++ app/GlobalSlice.ts
@@ -6,6 +6,7 @@ const globalSlice = createSlice({
     name: 'global',
     initialState: {
         isInboxPopupOpen: false,
+        isInboxDetailPopupOpen: false,
         isTaskPopupOpen: false,
         isLoading: false,
         isError: false,
@@ -14,14 +15,22 @@ const globalSlice = createSlice({
     reducers: {
         openInboxPopup(state) {
             state.isInboxPopupOpen = true
+            state.isInboxDetailPopupOpen = false
+            state.isTaskPopupOpen = false
+        },
+        openInboxDetailPopup(state) {
+            state.isInboxPopupOpen = false
+            state.isInboxDetailPopupOpen = true
             state.isTaskPopupOpen = false
         },
         openTaskPopup(state) {
             state.isInboxPopupOpen = false
+            state.isInboxDetailPopupOpen = false
             state.isTaskPopupOpen = true
         },
         closePopup(state) {
             state.isInboxPopupOpen = false
+            state.isInboxDetailPopupOpen = false
             state.isTaskPopupOpen = false
         },
         toggleLoading(state) {
@@ -34,6 +43,6 @@ const globalSlice = createSlice({
     },
 })
 
-export const { openInboxPopup, openTaskPopup, closePopup, toggleLoading, toggleError } = globalSlice.actions
+export const { openInboxPopup, openInboxDetailPopup, openTaskPopup, closePopup, toggleLoading, toggleError } = globalSlice.actions
 export const selectGlobalState = (state: RootState) => state.globalState  // SELECTOR
 export default globalSlice.reducer
