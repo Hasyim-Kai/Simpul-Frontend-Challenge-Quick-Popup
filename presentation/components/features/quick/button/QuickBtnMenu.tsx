48ae36fbc7e1155c6079a79f6ee40da04fd656e0
Hasyim Kai <ichihatsu1@gmail.com>
2023-01-04 09:14:22 +0700

    add functionality to quick btn

--- presentation/components/features/quick/button/QuickBtnMenu.tsx
+++ presentation/components/features/quick/button/QuickBtnMenu.tsx
@@ -1,17 +1,21 @@
 import Image from 'next/image'
 import { useState } from 'react'
+import { openInboxPopup, openTaskPopup, closePopup } from '../../../../../app/GlobalSlice';
+import { useAppDispatch } from '../../../../../app/store';
 import s from '../styles/quick-btn.module.scss'
 
 export default function QuickBtnMenu() {
+  const dispatch = useAppDispatch();
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
   const [selectedBtn, setSelectedBtn] = useState<string | null>(null)
   function handleMenu() {
     setIsMenuOpen((currentState) => !currentState)
     setSelectedBtn(null)
+    dispatch(closePopup())
   }
 
-  function openInbox() { setSelectedBtn('inbox') }
-  function openTask() { setSelectedBtn('task') }
+  function openInbox() { setSelectedBtn('inbox'); dispatch(openInboxPopup()) }
+  function openTask() { setSelectedBtn('task'); dispatch(openTaskPopup()) }
 
   return <section>
     <button className={`${selectedBtn === null ? s.quick_btn : s.quick_btn_hide}`} onClick={handleMenu}>
@@ -23,7 +27,7 @@ export default function QuickBtnMenu() {
         : selectedBtn === 'task' ? s.inbox_btn_inactive // if task btn selected
           : isMenuOpen ? s.inbox_btn : s.inbox_btn_hide}`} // if Quick btn clicked and menu opened, if not .. hide it
         onClick={openInbox}>
-        {selectedBtn === null && <span className={s.btn_text}>Inbox</span>}
+        {(selectedBtn === null && isMenuOpen) && <span className={s.btn_text}>Inbox</span>}
         <Image src={selectedBtn === 'inbox' ? './icons/chat_white.svg' : './icons/chat.svg'} alt="Inbox icon" width={26} height={26} />
       </button>
     </div>
@@ -33,7 +37,7 @@ export default function QuickBtnMenu() {
         : selectedBtn === 'inbox' ? s.task_btn_inactive
           : isMenuOpen ? s.task_btn : s.task_btn_hide}`}
         onClick={openTask}>
-        {selectedBtn === null && <span className={s.btn_text}>Task</span>}
+        {(selectedBtn === null && isMenuOpen) && <span className={s.btn_text}>Task</span>}
         <Image src={selectedBtn === 'task' ? './icons/book_white.svg' : './icons/book.svg'} alt="Task icon" width={26} height={26} />
       </button>
     </div>
